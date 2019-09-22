c1f650a5adc56df2a544ee152738289ddbb5b402
vutr <me@vutr.io>
2019-09-22 13:17:32 +0700

    prepare producer

--- producer/producer.py
+++ producer/producer.py
@@ -3,26 +3,45 @@ import random
 from os import getenv
 from logzero import logger as logs
 from kafka import KafkaProducer
+from aiohttp import web
 
-
-# give broker IP from docker
+# Setup Kafka Producer
 kafka_server = getenv('KAFKA_SERVER')
 kafka_topic = getenv('KAFKA_TOPIC')
 producer = KafkaProducer(bootstrap_servers=kafka_server)
 
-# continuous loop
-var = 1
-while var == 1:
+# Setup API Handler
+routes = web.RouteTableDef()
 
-    # generate a random integer
-    num = random.randint(0, 10)
+@routes.get('/')
+async def get_handler(request):
+    return web.Response(text="Hello, Application is UP!")
 
-    # message value and key must be raw bytes
-    num_bytes = bytes(str(num), encoding='utf-8')
 
-    # send to topic on broker
-    logs.info('TOPIC: %s -- Sending msg: %s', kafka_topic, num_bytes)
-    producer.send(kafka_topic, value=num_bytes, key=num_bytes)
+@routes.post('/post')
+async def post_handler(request):
+    pass
 
-    # wait 1 second
-    time.sleep(1)
+@routes.put('/api/stream_int')
+async def send_integer_stream(request):
+    """
+    Send continuous random integers (0-10) 10 times, for testing purpose only
+    """
+    count = 0
+    while count < 11:
+        num = random.randint(0, 10)
+        num_bytes = bytes(str(num), encoding='utf-8')
+        producer.send(kafka_topic, value=num_bytes, key=num_bytes)
+        count += 1
+        time.sleep(1)
+        logs.info('====== Count %s', count)
+
+    return web.Response(text='Done, Final count: {}'.format(count))
+
+
+app = web.Application()
+app.add_routes(routes)
+
+
+if __name__ == '__main__':
+    web.run_app(app)
