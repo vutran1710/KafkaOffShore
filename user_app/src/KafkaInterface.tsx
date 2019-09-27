8612e767f99783143ff99a55a52019cb9e9974e1
vutr <me@vutr.io>
2019-09-27 16:01:35 +0700

    styling user app

---
+++ user_app/src/KafkaInterface.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import * as kafka from './kafka.png'
+
+export interface Props {
+  loading: boolean;
+}
+
+const KafkaInterface = ({ loading }: Props) => {
+
+  const whichImage = loading ? 'http://giphygifs.s3.amazonaws.com/media/2wZswhaUnzAti/giphy.gif' : kafka
+  const kafkaImageClass = `kafka-image ${loading ? 'kafka-image__dark' : ''}`
+
+  return (
+    <div className="instruction">
+      <div className={kafkaImageClass} >
+        <img src={whichImage} height="120" width="120" />
+      </div>
+      <div className="title--main">
+        <span>K</span><span>A</span><span>F</span><span>K</span><span>A</span>
+      </div>
+      <div className="title--sub">
+        {loading ? 'PROCESSING...' : 'OFF THE SHORE'}
+      </div>
+    </div>
+  )
+}
+
+export default KafkaInterface
--- user_app/src/UserApp.tsx
+++ user_app/src/UserApp.tsx
@@ -1,37 +1,67 @@
 import React from 'react'
 import wretch from 'wretch'
+import KafkaInterface from './KafkaInterface'
 import './style.scss'
 
+type AppState = {
+  loading: boolean;
+  hitCount: number;
+  buttonText: string;
+}
 
-export default class UserApp extends React.Component {
-  state = {
-    loading: false,
-    hitCount: 0,
+export default class UserApp extends React.Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props)
+    this.state = {
+      loading: false,
+      hitCount: 0,
+      buttonText: "com' her!"
+    }
   }
 
   requestSendIntegerStream = () => {
-    this.setState({ loading: true, hitCount: this.state.hitCount + 1 })
+
+    this.setState({
+      loading: true,
+      hitCount: this.state.hitCount + 1,
+    })
+
     wretch('http://localhost:8080/api/stream_int').put().res().then(() => {
       this.setState({ loading: false })
     })
   }
 
+  changeText = (buttonText: string) => () => {
+    this.setState({ buttonText })
+  }
+
   render() {
+
+    const {
+      buttonText,
+      loading,
+    } = this.state
+
     return (
-      <div>
-        <h1>Hello Dear Users!</h1>
-        <hr />
-        {this.state.loading && (
-          <div>
-            <div>Sending....</div>
-            <hr />
+      <div className="kafka-container">
+        <div className="author">
+          by <b>vutr</b>, inspired by <i>Haruki Murakami</i>
+        </div>
+        <div className="info-container">
+          <KafkaInterface loading={loading} />
+        </div>
+        <div className="footer">
+          <div className="text--center">
+            <button
+              disabled={loading}
+              onClick={this.requestSendIntegerStream}
+              onMouseEnter={this.changeText('send kafka request?')}
+              onMouseLeave={this.changeText('click here!')}
+            >
+              {buttonText}
+            </button>
           </div>
-        )}
-        <button disabled={this.state.loading} onClick={this.requestSendIntegerStream}>
-          Send!
-        </button>
-        <hr />
-        Hit count: {this.state.hitCount}
+        </div>
       </div>
     )
   }
