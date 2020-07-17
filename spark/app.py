2fd9b4633ff0259eeb36dd05b117d6dc5df07936
vutr <me@vutr.io>
2020-07-17 17:36:49 +0700

    fix

--- spark/app.py
+++ spark/app.py
@@ -1,9 +1,31 @@
+""" Spark code
+"""
+from time import sleep
 from logzero import logger as log
 from pyspark import SparkContext
 
-sc = SparkContext(master="local[3]")
-data = [1, 2, 3, 4]
+
+sc = SparkContext(master="spark://localhost:7077", appName="app_example")
+sc.setLogLevel("INFO")
+data = [1, 2, 3, 5, 10]
 flow = sc.parallelize(data)
 
-result = flow.map(lambda x: x * 2).reduce(lambda x, y: x + y)
+
+def mapper(val):
+    """ A fake map function
+    """
+    log.info("this is a value before multiplication: %s", val)
+    newval = val * 2
+    count = 0
+
+    while count < 10:
+        sleep(1)
+        log.info("Fake processing...: %s", count)
+        count += 1
+
+    log.info("this is a value after multiplication: %s", newval)
+    return newval
+
+
+result = flow.map(mapper).collect()
 log.info(">>> Result = %s", result)
