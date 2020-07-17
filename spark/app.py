b8f7547f17668f828019252fa9f0dbfafb715ca7
vutr <me@vutr.io>
2020-07-17 14:33:10 +0700

    fix docker

--- spark/app.py
+++ spark/app.py
@@ -1,9 +1,9 @@
 from logzero import logger as log
 from pyspark import SparkContext
 
-sc = SparkContext(master="local[2]")
+sc = SparkContext(master="local[3]")
 data = [1, 2, 3, 4]
 flow = sc.parallelize(data)
 
-result = flow.map(lambda x: x * 2).reduce(lambda x, y: x + y).collect()
-log.info('>>> Result = %s', result)
+result = flow.map(lambda x: x * 2).reduce(lambda x, y: x + y)
+log.info(">>> Result = %s", result)
