76ae06de8e9cc93d080611e8e02975c1ff6d0f2a
vutr <me@vutr.io>
2020-07-19 16:37:22 +0700

    fix

--- spark/app.py
+++ spark/app.py
@@ -7,7 +7,10 @@ from pyspark import SparkContext
 
 sc = SparkContext(master="spark://localhost:7077", appName="app_example")
 sc.setLogLevel("INFO")
-data = [1, 2, 3, 5, 10]
+sc.set("spark.executor.memory", "512m")
+sc.set("spark.executor.cores", "1")
+
+data = [1, 2, 3, 5]
 flow = sc.parallelize(data)
 
 
@@ -18,9 +21,9 @@ def mapper(val):
     newval = val * 2
     count = 0
 
-    while count < 10:
+    for _ in range(5):
         sleep(1)
-        log.info("Fake processing...: %s", count)
+        log.warning("Fake processing...: %s", count)
         count += 1
 
     log.info("this is a value after multiplication: %s", newval)
