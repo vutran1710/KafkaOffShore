a957e641c68e43a7f74c1fafcf5de612f6d3654f
vutr <me@vutr.io>
2020-07-20 14:41:53 +0700

    change spark-main

--- spark/main.py
+++ spark/main.py
@@ -1,35 +1,16 @@
-from os import environ
-import sys
-from pyspark import SparkContext, SparkConf
-from pyspark.streaming import StreamingContext
-from pyspark.streaming.kafka import KafkaUtils
+from logzero import logger as log
+from streamer import SparkStreaming
+from consumer import Consumer
 from config import AppConfig
 
 
 if __name__ == "__main__":
 
     cfg = AppConfig.load()
+    log.debug(cfg)
 
-    spark_cfg = SparkConf()
-    spark_cfg.setAppName("app")
-    spark_cfg.setMaster("spark://localhost:7077")
-    spark_cfg.set("spark.executor.memory", "512m")
-    spark_cfg.set("spark.executor.cores", "1")
+    # SparkStreaming(cfg)
+    consumer = Consumer(cfg)
 
-    sc = SparkContext(conf=spark_cfg)
-    ssc = StreamingContext(sc, 2)
-
-    kvs = KafkaUtils.createDirectStream(
-        ssc, [cfg.TOPIC], {"metadata.broker.list": cfg.BROKERS},
-    )
-
-    lines = kvs.map(lambda x: x[1])
-    counts = (
-        lines.flatMap(lambda line: line.split(" "))
-        .map(lambda word: (word, 1))
-        .reduceByKey(lambda a, b: a + b)
-    )
-
-    counts.pprint()
-    ssc.start()
-    ssc.awaitTermination()
+    consumer.create_socket_host()
+    consumer.read_stream()
