a7d48b196949ee9421444c26cd9dafee020c4910
vutr <me@vutr.io>
2020-07-27 21:19:44 +0700

    clean up

--- spark_job/src/main/scala/StreamJob.scala
+++ spark_job/src/main/scala/StreamJob.scala
@@ -5,17 +5,6 @@ import org.apache.spark.streaming._
 import org.apache.spark.streaming.kafka010._
 import org.apache.log4j.{LogManager, Logger}
 
-/**
- * Consumes messages from one or more topics in Kafka and does wordcount.
- * Usage: DirectKafkaWordCount <brokers> <topics>
- * <brokers> is a list of one or more Kafka brokers
- * <groupId> is a consumer group name to consume from topics
- * <topics> is a list of one or more kafka topics to consume from
- *
- * Example:
- * $ bin/run-example streaming.DirectKafkaWordCount broker1-host:port,broker2-host:port \
- * consumer-group topic1,topic2
- */
 
 object StreamJob  {
   var log: Logger = LogManager.getLogger(this.getClass.getName)
@@ -56,7 +45,7 @@ object StreamJob  {
     rdds.foreachRDD(rdd => {
       rdd.collect().foreach(item => {
         val (key, value) = item
-        this.log.info(s"$key=$value")
+        this.log.info(s">>>>> $key = $value")
       })
     })
 
