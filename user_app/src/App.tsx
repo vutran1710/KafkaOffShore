4c4b2e96cca4c7897c6666574f852ce564ec208d
vutr <me@vutr.io>
2020-07-29 15:48:47 +0700

    fix confusing dirname

--- user_app/src/App.tsx
+++ user_app/src/App.tsx
@@ -4,11 +4,11 @@ import {
   Route,
   Switch,
 } from 'react-router-dom'
-import Articles from './react_components/Articles'
-import KafkaBook from './react_components/KafkaBook'
-import Bookmarker from './react_components/Bookmarker'
-import StreamForm from './react_components/StreamForm'
-import TableOfContent from './react_components/TableOfContent'
+import Articles from './components/Articles'
+import KafkaBook from './components/KafkaBook'
+import Bookmarker from './components/Bookmarker'
+import StreamForm from './components/StreamForm'
+import TableOfContent from './components/TableOfContent'
 
 
 const App = () => (
