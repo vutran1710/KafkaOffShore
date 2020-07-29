160e8ddeee08dea0b35f8856ac4a3b350cf732c0
vutr <me@vutr.io>
2020-07-29 17:24:54 +0700

    Add tracking

--- user_app/src/components/Articles.tsx
+++ user_app/src/components/Articles.tsx
@@ -1,40 +1,74 @@
 import React from 'react'
-import posts from '../utils/article_data'
+import post_mock_data from '../utils/article_data'
 
-type AppState = {
-  cursorPosition: number;
-  readTime: number;
+
+type PostData = {
+  id: number
+  content: string
+  image?: string
 }
 
-type Post = {
-  id: number;
-  content: string;
-  image?: string;
+type State = {
+  scrollTop: number;
 }
 
-export default class Articles extends React.Component<{}, AppState> {
-  state = {
-    cursorPosition: 0,
-    readTime: 0,
+type Props = null
+
+export default class Articles extends React.Component<Props, State> {
+  myRef = React.createRef<HTMLDivElement>()
+  state = { scrollTop: 0 }
+  ticker = null
+  lastScroll = null
+
+  componentDidMount() {
+    // Intervally check the scroll position every 1.2s,
+    this.ticker = setInterval(this.checkIsReading, 1200)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.ticker)
+    this.ticker = null
+  }
+
+  checkIsReading = () => {
+    // if this.state.scrollTop is not changing,
+    // that means User is reading the in-view post
+    if (this.lastScroll === this.state.scrollTop) {
+      const whichPost = Math.round(this.lastScroll / 500 + 1)
+      console.log(`User is reading post: #${whichPost}`)
+    } else {
+      this.lastScroll = this.state.scrollTop
+    }
+  }
+
+  onScroll = () => {
+    const scrollTop = this.myRef.current.scrollTop
+    this.lastScroll = this.state.scrollTop
+    this.setState({
+      scrollTop: scrollTop
+    })
   }
 
   render() {
-    const { cursorPosition, readTime } = this.state
     return (
       <div className="article-container">
         <div className="article-container--header">
           <h2>Stories</h2>
         </div>
-        <div className="article-container--body">
-          {posts.map((p: Post) => (
-            <div className="article-container--post article-container--post__container" key={p.id}>
+        <div
+          className="article-container--body"
+          ref={this.myRef}
+          onScroll={this.onScroll}
+        >
+          {(post_mock_data as Array<PostData>).map(post => (
+            <div className="article-container--post article-container--post__container" key={post.id}>
               <div className="article-container--post__header">
-                ... #{p.id}
+                ... #{post.id}
               </div>
               <div className="article-container--post__body">
-                <p>{p.content}</p>
+                <p>{post.content}</p>
                 <div>
-                  <img alt={`${p.id}-image`} src={p.image} />
+                  <img alt={`${post.id}-image`} src={post.image} />
                 </div>
               </div>
             </div>
